c2dd3f58a319d54e7bf64dbd3b11d25c6fbff806
Gabriel Francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2023-11-23 19:59:47 -0300

    implementação inicial do aluguel

--- src/useCases/rent.ts
+++ src/useCases/rent.ts
@@ -1,15 +1,15 @@
-import { Customer } from "./customer"
-import { TVehicle, Vehicle } from "./vehicle"
+import { Customer } from "./customer";
+import { TVehicle, Vehicle } from "./vehicle";
 
 export class Rent {
-    private _customer: Customer
-    private _vehicle: Vehicle
-    private _rentalDate: Date
-    private _devolutionDate: Date
-    private _dailyRate: number
-    private _daysRented: number
-    private _vehicleType: TVehicle
-    private _surcharge: number
+    private _customer: Customer;
+    private _vehicle: Vehicle;
+    private _rentalDate: Date;
+    private _devolutionDate: Date;
+    private _dailyRate: number;
+    private _daysRented: number;
+    private _vehicleType: TVehicle;
+    private _surcharge: number;
 
     constructor(
         customer: Customer,
@@ -21,82 +21,82 @@ export class Rent {
         vehicleType: TVehicle,
         surcharge: number
     ) {
-        this._customer = customer
-        this._vehicle = vehicle
-        this._rentalDate = rentalDate
-        this._devolutionDate = devolutionDate
-        this._dailyRate = dailyRate
-        this._daysRented = daysRented
-        this._vehicleType = vehicleType
-        this._surcharge = surcharge
+        this._customer = customer;
+        this._vehicle = vehicle;
+        this._rentalDate = rentalDate;
+        this._devolutionDate = devolutionDate;
+        this._dailyRate = dailyRate;
+        this._daysRented = daysRented;
+        this._vehicleType = vehicleType;
+        this._surcharge = surcharge;
     }
 
     get customer(): Customer {
-        return this._customer
+        return this._customer;
     }
 
     set customer(newCustomer: Customer) {
-        this._customer = newCustomer
+        this._customer = newCustomer;
     }
 
     get vehicle(): Vehicle {
-        return this._vehicle
+        return this._vehicle;
     }
 
     set vehicle(newVehicle: Vehicle) {
-        this._vehicle = newVehicle
+        this._vehicle = newVehicle;
     }
 
     get rentalDate(): Date {
-        return this._rentalDate
+        return this._rentalDate;
     }
 
     set rentalDate(newRentalDate: Date) {
-        this._rentalDate = newRentalDate
+        this._rentalDate = newRentalDate;
     }
 
     get devolutionDate(): Date {
-        return this._devolutionDate
+        return this._devolutionDate;
     }
 
     set devolutionDate(newDevolutionDate: Date) {
-        this._devolutionDate = newDevolutionDate
+        this._devolutionDate = newDevolutionDate;
     }
 
     get dailyRate(): number {
-        return this._dailyRate
+        return this._dailyRate;
     }
 
     set dailyRate(newDailyRate: number) {
-        this._dailyRate = newDailyRate
+        this._dailyRate = newDailyRate;
     }
 
     get daysRented(): number {
-        return this._daysRented
+        return this._daysRented;
     }
 
     set daysRented(newDaysRented: number) {
-        this._daysRented = newDaysRented
+        this._daysRented = newDaysRented;
     }
 
     get vehicleType(): TVehicle {
-        return this._vehicleType
+        return this._vehicleType;
     }
 
     set vehicleType(newVehicleType: TVehicle) {
-        this._vehicleType = newVehicleType
+        this._vehicleType = newVehicleType;
     }
 
     get surcharge(): number {
-        return this._surcharge
+        return this._surcharge;
     }
 
     set surcharge(newSurcharge: number) {
-        this._surcharge = newSurcharge
+        this._surcharge = newSurcharge;
     }
 
     calculateTotalValue(): number {
-        return this._dailyRate * this._daysRented
+        return this._dailyRate * this._daysRented;
     }
 
 }
