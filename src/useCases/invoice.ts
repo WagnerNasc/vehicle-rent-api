c047c9d95827dec48d2ede544c0423c8506b3f60
Gabriel Francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2023-11-24 11:50:50 -0300

    método para gerar fatura

--- src/useCases/invoice.ts
+++ src/useCases/invoice.ts
@@ -1,26 +1,25 @@
 import { Customer } from "./customer";
+import { Rent } from "./rent";
 import { Vehicle } from "./vehicle";
 
 export class Invoice {
-    private _customer: Customer;
-    private _vehicle: Vehicle;
 
-    constructor(user: Customer, vehicle: Vehicle) {
-        this._customer = user;
-        this._vehicle = vehicle;
-    }
+    static generateInvoice(rent: Rent): string {
 
-    generateInvoice(): string {
         return `
         ============ FATURA DO CLIENTE ============
-        Nome do cliente: ${this._customer.name}
-        CPF: ${this._customer.cpf}
-        CNH: ${this._customer.driverLicense}
+        Nome do cliente: ${rent.customer.name}
+        CPF: ${rent.customer.cpf}
+        CNH: ${rent.customer.driverLicense}
         ------------ DADOS DO VEÍCULO -------------
-        Placa: ${this._vehicle.plate}
-        Tipo: ${this._vehicle.type}
-        Modelo: ${this._vehicle.model}
-        Valor do aluguel: ${this._vehicle.valueRental}
+        Placa: ${rent.vehicle.plate}
+        Tipo: ${rent.vehicle.type}
+        Modelo: ${rent.vehicle.model}
+        Diária: ${rent.vehicle.dailyRental}
+        ------------ DADOS DO ALUGUEL -------------
+        Data de aluguel: ${rent.rentalDate}
+        Data de devolução: ${rent.devolutionDate}
+        Valor do aluguel: ${rent.valueRental}
         `;
     }
 }
